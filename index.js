3ae90e7b74354de5d68effad7765c8773eff2aa4
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-12-09 16:09:12 -0800

    buncha stuff

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ var iMessage = require('imessage');
 var im = new iMessage();
  
 // Get all recipients 
-im.getRecipients(function(err, res) {
-  console.log(err);
-  res.forEach(function(e) {
-    console.log(e);
-  });
-})
+//im.getRecipients(function(err, res) {
+//  console.log(err);
+//  res.forEach(function(e) {
+//    console.log(e);
+//  });
+//})
+//
+
+const app = require('express')();
+const bodyParser = require('body-parser')
+const through = require('through');
+
+app.use(bodyParser.json());
+
+app.post('/events', function(req, res, next) {
+  console.log('got stuff');
+  console.log(req.body);
+  res.send('ok');
+});
+
+app.listen(4005);
+console.log('listening on 4005');
