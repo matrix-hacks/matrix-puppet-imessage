23996fcf9d33a5aa479c90acd8c7c38f0dac41ab
Michael New <mikenew12@gmail.com>
2019-06-21 14:42:44 -0700

    Fix attachemnts failing to send when there are spaces in the filename

--- src/find-attachment.js
+++ src/find-attachment.js
@@ -4,7 +4,7 @@ const config = require('../config.json');
 
 module.exports = function(id, name) {
   return new Promise(function(resolve, reject) {
-    exec(`find ${config.attachmentsDir}/*/*/${name}/${id}`, function(err, stdout, stderr) {
+    exec(`find ${config.attachmentsDir}/*/*/"${name}"/"${id}"`, function(err, stdout, stderr) {
       if (err) {
         console.error(stderr);
         return reject(err);
